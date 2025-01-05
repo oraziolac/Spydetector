2b88423b825c5cec432a76708e0be27cb75ed7b8
oraziolac <167475591+oraziolac@users.noreply.github.com>
2025-01-05 21:14:08 +0100

    Update home.vue

--- app/backend/src/views/home.vue
+++ app/backend/src/views/home.vue
@@ -1,17 +1,17 @@
-<template>
+ctor<template>
     <div class="backend-content" id="content">
       <div class="column col-8 col-xs-12">
           <div class="container">
             <h3 class="s-title">Getting started</h3>
             <br />
-            <p>SpyGuard is a forked and enhanced version of TinyCheck, an application developed by Kaspersky. SpyGuard's main objective is to detect signs of compromise by monitoring network flows transmitted by a device.</p>
-            <p>As it uses WiFi, SpyGuard can be used against a wide range of devices, such as smartphones, laptops, IOTs or workstations. To do its job, the analysis engine of SpyGuard is using Indicators of Compromise (IOCs), anomaly detection and is supported by Suricata. </p>
+            <p>Spydetector is a forked and enhanced version of TinyCheck, Spydetector's main objective is to detect signs of compromise by monitoring network flows transmitted by a device.</p>
+            <p>As it uses WiFi, Spydetector can be used against a wide range of devices, such as smartphones, laptops, IOTs or workstations. To do its job, the analysis engine of Spydetector is using Indicators of Compromise (IOCs), anomaly detection and is supported by Suricata. </p>
 
-            <p>This backend lets you configure your SpyGuard instance. You can push some IOCs for detection and whitelist elements which can be seen during legit communications in order to prevent false positives.</p>
+            <p>This backend lets you configure your Spydetector instance. You can push some IOCs for detection and whitelist elements which can be seen during legit communications in order to prevent false positives.</p>
             <p>_</p>
           </div>
           <div class="backend-footer container grid-lg" id="copyright">
-             <p>For any question, bug report or feedback, please contact the <a href="mailto:spyguard@protonmail.com" target="_blank">SpyGuard's Team</a> or open an issue on the <a href="https://github.com/SpyGuard/spyguard/issues" target="_blank">SpyGuard Github repository</a>.</p>
+             <p>For any question, bug report or feedback, please contact the <a href="mailto:Webspydetector@protonmail.com" target="_blank">Spydetector's Team</a> or open an issue on the <a href="https://github.com/" target="_blank">Spedetector Github repository</a>.</p>
           </div>
       </div>
     </div>
